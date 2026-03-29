92e78d10b5989008733d3b558319b9ffccc3c7a8
Clayel <videogamer0907@gmail.com>
2026-03-29 19:15:37 -0400

    add a way to freeze the craft

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+![Toolbar Icon](https://github.com/Clayell/CelestialBodyMover/blob/master/GameData/CelestialBodyMover/PluginData/ToolbarIcons/button-64.png)
+
 [![GitHub Downloads (all assets, latest release)](https://img.shields.io/github/downloads/Clayell/CelestialBodyMover/latest/total?label=downloads%20(latest))](https://github.com/Clayell/CelestialBodyMover/releases/latest)
 [![GitHub Downloads (all assets, all releases)](https://img.shields.io/github/downloads/Clayell/CelestialBodyMover/total?label=downloads%20(total))](https://github.com/Clayell/CelestialBodyMover/releases)
 
--- Source/CelestialBodyMover.cs
+++ Source/CelestialBodyMover.cs
@@ -1,10 +1,12 @@
-﻿using KSP.UI.Screens;
+﻿//using BackgroundThrust;
+using ClickThroughFix;
+using HarmonyLib;
+using KSP.UI.Screens;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using ToolbarControl_NS;
 using UnityEngine;
-using ClickThroughFix;
-using System.Linq;
 
 namespace CelestialBodyMover
 {
@@ -13,19 +15,41 @@ namespace CelestialBodyMover
     {
         void Start()
         {
-            ToolbarControl.RegisterMod("CBM", "CelestialBodyMover");
+            ToolbarControl.RegisterMod("CelestialBodyMover", "CelestialBodyMover");
         }
     }
 
-    [KSPAddon(KSPAddon.Startup.FlightAndKSC, false)]
+    internal static class Util
+    {
+        internal static void Log(string message, string prefix = "[CelestialBodyMover]")
+        {
+            UnityEngine.Debug.Log($"{prefix}: {message}"); // KSPLog.print does the same thing
+        }
+
+        internal static void LogWarning(string message, string prefix = "[CelestialBodyMover]")
+        {
+            UnityEngine.Debug.LogWarning($"{prefix}: {message}");
+        }
+
+        internal static void LogError(string message, string prefix = "[CelestialBodyMover]")
+        {
+            UnityEngine.Debug.LogError($"{prefix}: {message}");
+        }
+    }
+
+    [KSPAddon(KSPAddon.Startup.AllGameScenes, false)]
     public class CelestialBodyMover : MonoBehaviour
     {
         ToolbarControl toolbarControl = null;
 
         GUISkin skin;
 
-        bool isWindowOpen = false;
-        bool isActive = true;
+        bool isWindowOpen = false; // hide on first start-up
+        bool isKSPGUIActive = true; // for some reason, this initially only turns to true when you turn off and on the KSP GUI
+
+        internal static bool isActive = false;
+
+        Vector3d currentPos = Vector3d.zero;
 
         Rect mainRect = new Rect(100, 100, -1, -1);
 
@@ -48,6 +72,9 @@ namespace CelestialBodyMover
         void Awake()
         {
             skin = (GUISkin)GUISkin.Instantiate(HighLogic.Skin);
+
+            GameEvents.onShowUI.Add(KSPShowGUI);
+            GameEvents.onHideUI.Add(KSPHideGUI);
         }
 
         void Start()
@@ -59,9 +86,16 @@ namespace CelestialBodyMover
         {
             Destroy(toolbarControl);
             toolbarControl = null;
+
+            GameEvents.onShowUI.Remove(KSPShowGUI);
+            GameEvents.onHideUI.Remove(KSPHideGUI);
         }
 
         private void ToggleWindow() => isWindowOpen = !isWindowOpen;
+
+        private void KSPShowGUI() => isKSPGUIActive = true;
+
+        private void KSPHideGUI() => isKSPGUIActive = false;
 
         void OnGUI()
         {
@@ -77,7 +111,15 @@ namespace CelestialBodyMover
 
         void Update()
         {
-            GetThrust();
+            //GetThrust();
+            MakeVesselStationary();
+
+            CheatOptions.InfinitePropellant = true;
+            CheatOptions.InfiniteElectricity = true;
+            CheatOptions.NoCrashDamage = true;
+            CheatOptions.UnbreakableJoints = true;
+            CheatOptions.IgnoreEVAConstructionMassLimit = true;
+            CheatOptions.IgnoreKerbalInventoryLimits = true;
         }
 
         private void ClampToScreen(ref Rect rect)
@@ -87,65 +129,251 @@ namespace CelestialBodyMover
             rect = new Rect(left, top, rect.width, rect.height);
         }
 
-        static void Log(string message, string prefix = "[CelestialBodyMover]")
-        {
-            Debug.Log($"{prefix}: {message}");
-        }
-
         private void MakeMainWindow(int id)
         {
-            if (GUILayout.Button(isActive ? "Deactivate" : "Activate"))
+            if (HighLogic.LoadedScene == GameScenes.FLIGHT)
             {
-                isActive = !isActive;
-                Log(isActive ? "Activated" : "Deactivated");
+                if (GUILayout.Button(isActive ? "Deactivate" : "Activate"))
+                {
+                    isActive = !isActive;
+                    if (FlightGlobals.ActiveVessel != null)
+                    {
+                        Vessel vessel = FlightGlobals.ActiveVessel;
+
+                        currentPos = vessel.GetWorldPos3D();
+                    }
+
+
+                    Util.Log(isActive ? "Activated" : "Deactivated");
+                }
+            }
+            else
+            {
+                GUILayout.Label("Not flight scene!");
             }
             GUI.DragWindow();
+        }
+        private void MakeVesselStationary()
+        {
+            Vessel vessel = FlightGlobals.ActiveVessel;
+
+            if (vessel == null)
+                return;
+
+            if (isActive)
+            {
+                vessel.SetWorldVelocity(Vector3d.zero);
+                vessel.SetPosition(currentPos);
+            }
         }
 
         private void GetThrust()
         {
-            if (!isActive) return;
+            if (!isActive || HighLogic.LoadedScene != GameScenes.FLIGHT) return;
 
             Vessel vessel = FlightGlobals.ActiveVessel;
-            List<Part> parts = vessel.parts.Where(p => p.State == PartStates.ACTIVE).ToList();
+            Vector3d thrustVector = Vector3d.zero;
+            Vector3d thrustVector2 = Vector3d.zero;
+            Vector3d thrustVector3 = Vector3d.zero;
 
-            // thrust code taken from https://github.com/MuMech/MechJeb2/blob/dev/MechJeb2/VesselState.cs
-            Vector3d thrustCurrent = Vector3d.zero;
             Vector3d vesselForward = vessel.GetTransform().up;
 
-            for (int i1 = 0; i1 < parts.Count; i1++)
+            //vessel.GetHeightFromTerrain();
+
+            if (TimeWarp.CurrentRate > 1d)
             {
-                Part part = parts[i1];
-                for (int i2 = 0; i2 < part.Modules.Count; i2++)
+                //BackgroundThrustVessel bVessel = vessel.FindVesselModuleImplementing<BackgroundThrustVessel>();
+                //thrustVector = bVessel.Thrust;
+                thrustVector = Vector3d.zero;
+            }
+            else
+            {
+                // thrust code taken from https://github.com/MuMech/MechJeb2/blob/dev/MechJeb2/VesselState.cs
+
+                List<Part> parts = vessel.parts.Where(p => p.State == PartStates.ACTIVE).ToList();
+
+                for (int i1 = 0; i1 < parts.Count; i1++)
                 {
-                    PartModule pm = part.Modules[i2];
-                    if (!pm.isEnabled)
+                    Part part = parts[i1];
+
+                    thrustVector3 += part.force;
+
+                    for (int i2 = 0; i2 < part.Modules.Count; i2++)
                     {
-                        continue;
+                        PartModule pm = part.Modules[i2];
+                        if (!pm.isEnabled)
+                        {
+                            continue;
+                        }
+
+                        if (pm is ModuleEngines e)
+                        {
+                            for (int i3 = 0; i3 < e.thrustTransforms.Count; i3++)
+                            {
+                                Transform transform = e.thrustTransforms[i3];
+                                // The rotation makes a +z vector point in the direction that molecules are ejected
+                                // from the engine.  The resulting thrust force is in the opposite direction.
+                                Vector3d thrustDirectionVector = -transform.forward;
+
+                                double cosineLosses = Vector3d.Dot(thrustDirectionVector, vesselForward);
+                                float thrustTransformMultiplier = e.thrustTransformMultipliers[i3];
+                                double tCurrentThrust = e.finalThrust * thrustTransformMultiplier;
+                                Vector3d transformThrust = tCurrentThrust * cosineLosses * thrustDirectionVector;
+                                Vector3d transformThrust2 = tCurrentThrust * cosineLosses * vesselForward;
+
+                                thrustVector += transformThrust;
+                                thrustVector2 += transformThrust2;
+
+                                Util.Log($"transformThrust: {transformThrust}, transformThrust2: {transformThrust2}, thrustVector: {thrustVector}, thrustVector2: {thrustVector2}, thrustDirectionVector: {thrustDirectionVector}, cosineLosses: {cosineLosses}, tCurrentThrust: {tCurrentThrust}");
+                            }
+                        }
                     }
 
-                    if (pm is ModuleEngines e)
+                    for (int i2 = 0; i2 < part.forces.Count; i2++)
                     {
-                        for (int i3 = 0; i3 < e.thrustTransforms.Count; i3++)
-                        {
-                            Transform transform = e.thrustTransforms[i3];
-                            // The rotation makes a +z vector point in the direction that molecules are ejected
-                            // from the engine.  The resulting thrust force is in the opposite direction.
-                            Vector3d thrustDirectionVector = -transform.forward;
-
-                            double cosineLosses = Vector3d.Dot(thrustDirectionVector, vesselForward);
-                            float thrustTransformMultiplier = e.thrustTransformMultipliers[i3];
-                            double tCurrentThrust = e.finalThrust * thrustTransformMultiplier;
-
-                            thrustCurrent += tCurrentThrust * cosineLosses * thrustDirectionVector;
-                        }
+                        thrustVector3 += part.forces[i2].force;
                     }
                 }
             }
 
-            double thrustCurrentMag = Vector3d.Dot(thrustCurrent, vesselForward);
+            double currentUT = Planetarium.GetUniversalTime();
 
-            Log($"thrustCurrentMag: {thrustCurrentMag}, thrustCurrent: {thrustCurrent}, vesselForward: {vesselForward}");
+            double thrustMagnitude = thrustVector.magnitude;
+            double thrustMagnitude2 = thrustVector2.magnitude;
+            double thrustMagnitude3 = thrustVector3.magnitude;
+
+            Vector3d thrustNormal = thrustVector.normalized;
+
+            CelestialBody body = vessel.mainBody;
+
+            Orbit bodyOrbit = body.orbit;
+            Vector3d vesselPos = vessel.GetWorldPos3D();
+            Vector3d planetPos = body.position;
+            Vector3d toPlanet = (planetPos - vesselPos).normalized;
+            double alignment = Vector3d.Dot(thrustNormal, toPlanet);
+            double effectiveThrust = thrustMagnitude * alignment;
+            Vector3d forceOnPlanet = -thrustNormal * effectiveThrust;
+            double totalMass = body.Mass + vessel.totalMass;
+            Vector3d accel = forceOnPlanet / totalMass;
+            Vector3d deltaV = accel * Time.fixedDeltaTime;
+            Vector3d position = bodyOrbit.getRelativePositionAtUT(currentUT);
+            Vector3d velocity = bodyOrbit.getOrbitalVelocityAtUT(currentUT);
+
+            Vector3d newVelocity = velocity + deltaV;
+
+            Vector3d r = vesselPos - planetPos;
+            Vector3d torque = Vector3d.Cross(r, forceOnPlanet);
+            Vector3d axis = body.angularVelocity.normalized;
+            double torqueAlongAxis = Vector3d.Dot(torque, axis);
+            Vector3d axisTorque = axis * torqueAlongAxis;
+
+            double I = (0.4 * body.Mass * body.Radius * body.Radius) + vessel.totalMass * r.magnitude * r.magnitude;
+
+            double angularAccel = torqueAlongAxis / I;
+
+            double dt = Time.fixedDeltaTime;
+
+            // Update magnitude along axis
+            Vector3d newAngularVelocity = body.angularVelocity + axis * (angularAccel * dt);
+            double omega = newAngularVelocity.magnitude;
+            double newPeriod = (2.0 * Math.PI) / omega;
+            double origPeriod = (2.0 * Math.PI) / body.angularVelocity.magnitude;
+
+            // need to make sure the vessel is touching the ground (not just splashed)
+
+            Util.Log($"TimeWarp.CurrentRate: {TimeWarp.CurrentRate}, thrustVector: {thrustVector}, thrustVector2: {thrustVector2}, thrustVector3: {thrustVector3}, thrustMagnitude: {thrustMagnitude}, thrustMagnitude2: {thrustMagnitude2}, thrustMagnitude3: {thrustMagnitude3}, bodyPos: {body.position}, bodyAngVel: {body.angularVelocity}, body.zUpAngularVelocity: {body.zUpAngularVelocity}, rotationPeriod: {body.rotationPeriod}, solarDayLength: {body.solarDayLength}, rotation: {body.rotation}, rotationAngle: {body.rotationAngle}, referenceBody: {bodyOrbit.referenceBody.bodyName}, GetRelativeVel: {bodyOrbit.GetRelativeVel()}, orbitalSpeed: {bodyOrbit.orbitalSpeed}, GetFrameVel().magnitude: {bodyOrbit.GetFrameVel().magnitude}, transformRight: {body.transformRight}, transformUp: {body.transformUp}");
+            Util.Log($"thrustVector.normalized: {thrustVector.normalized} vesselForward: {vesselForward} dot: {Vector3d.Dot(thrustVector.normalized, vesselForward)}, vesselPos: {vesselPos}, planetPos: {planetPos}, toPlanet: {toPlanet}, alignment: {alignment}");
+            Util.Log($"velocity: {velocity}, newVelocity: {newVelocity}, forceOnPlanet: {forceOnPlanet}, origPeriod: {origPeriod}, newPeriod: {newPeriod}");
+        }
+    }
+
+    //[HarmonyPatch(typeof(Vessel))]
+    //public static class VesselPatches
+    //{
+    //    [HarmonyPatch(nameof(Vessel.UpdateAcceleration))]
+    //    [HarmonyPrefix]
+    //    internal static bool Prefix_UpdateAcceleration(Vessel __instance, double fdtRecip, bool fromUpdate)
+    //    {
+    //        if (!CelestialBodyMover.isActive)
+    //        {
+    //            return true;
+    //        }
+
+    //        //Util.Log($"Prefix_UpdateAcceleration called");
+
+    //        bool flag = __instance.mainBody.rotates && __instance.mainBody.inverseRotation;
+
+    //        if (__instance.loaded && !__instance.packed)
+    //        {
+    //            __instance.acceleration.Zero();
+    //            __instance.acceleration_immediate.Zero();
+    //            __instance.perturbation.Zero();
+    //            __instance.perturbation_immediate.Zero();
+    //            __instance.geeForce = 0d;
+    //            __instance.specificAcceleration = 0d;
+    //            __instance.geeForce_immediate = 0d;
+
+    //            __instance.lastVel = __instance.obt_velocity;
+
+    //            __instance.frameWasRotating = flag;
+    //            __instance.krakensbaneAcc.Zero();
+    //            __instance.lastBody = __instance.orbit.referenceBody;
+
+    //            return false;
+    //        }
+    //        else
+    //        {
+    //            return true;
+    //        }
+    //    }
+
+    //    [HarmonyPatch(nameof(Vessel.UpdatePosVel))]
+    //    [HarmonyPostfix]
+    //    internal static void Postfix_UpdatePosVel(Vessel __instance)
+    //    {
+    //        if (!CelestialBodyMover.isActive)
+    //        {
+    //            return;
+    //        }
+
+    //        //Util.Log($"Postfix_UpdatePosVel called");
+
+    //        __instance.obt_velocity.Zero();
+    //        __instance.obt_speed = 0d;
+    //        __instance.srf_velocity.Zero();
+    //        __instance.verticalSpeed = 0d;
+    //        __instance.srfSpeed = 0.0;
+    //        __instance.horizontalSrfSpeed = 0.0;
+    //        __instance.srf_vel_direction.Zero();
+    //    }
+    //}
+
+    //[HarmonyPatch(typeof(OrbitDriver))]
+    //public static class OrbitDriverPatches
+    //{
+    //    [HarmonyPatch(nameof(OrbitDriver.UpdateOrbit))]
+    //    [HarmonyPrefix]
+    //    internal static bool Prefix_UpdateOrbit(OrbitDriver __instance)
+    //    {
+    //        if (!CelestialBodyMover.isActive || __instance != FlightGlobals.ActiveVessel?.orbitDriver)
+    //        {
+    //            return true;
+    //        }
+
+    //        //Util.Log($"Prefix_UpdateOrbit called");
+
+    //        __instance.lastMode = OrbitDriver.UpdateMode.IDLE;
+    //        return true;
+    //    }
+    //}
+
+    [KSPAddon(KSPAddon.Startup.Instantly, true)]
+    public class HarmonyPatcher : MonoBehaviour
+    {
+        internal void Start()
+        {
+            var harmony = new Harmony("CelestialBodyMover.HarmonyPatcher");
+            harmony.PatchAll();
         }
     }
 }
